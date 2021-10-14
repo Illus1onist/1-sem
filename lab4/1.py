be641af793b9c678abf9c9b03270944dc3b7d826
Illus1onist <grguliy@mail.ru>
2021-10-14 11:40:59 +0300

    Added comments

--- lab4/1.py
+++ lab4/1.py
@@ -18,32 +18,39 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 global a,k
-a=1
-k=0
-score=0
-N=40
-b=np.eye(N,5)
-c=np.eye(N,3)
+a=1             #счетчик времени
+k=0             #количество шариков на экране
+score=0         #счет
+N=40            #максимальное количество шариков
+b=np.eye(N,5)   #массив для координат, радиуса и скорости кажого шарика
+c=np.eye(N,3)   #массив для цвета этих шариков
+
+
+#функция для рисования нового шарика с случайными
 def new_ball():
-    '''рисует новый шарик '''
+
     global x,y,r,k,vx,vy
     x = randint(100, 1100)
     y = randint(100, 800)
     r = randint(50, 100)
 
-    diapasonx=list(range(-25,-10))+list(range(10,25))
+    diapasonx=list(range(-25,-10))+list(range(10,25))           #диапазон скоростей
     diapasony = list(range(-25, -10)) + list(range(10, 25))
+
     vx = diapasonx[randint(0,len(diapasonx)-1)]
     vy = diapasony[randint(0,len(diapasony)-1)]
+
     color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+
+    circle(screen, color, (x, y), r)    #создание шара
+    #запись всех параметров в массивы
     b[k][0] = x
     b[k][1] = y
     b[k][2] = r
     b[k][3] = vx
     b[k][4] = vy
     c[k]=color
-
+    #увеличение количества шариков
     k=k+1
 
 
@@ -57,8 +64,10 @@ screen.blit(text, (250, 20))
 
 while not finished:
     clock.tick(FPS)
-
+    #часы
     a = a + 1
+
+    #реализация движения шаров
     for i in range(k):
         b[i][0] = b[i][0]+b[i][3]*0.1
         b[i][1] = b[i][1]+b[i][4]*0.1
@@ -66,10 +75,10 @@ while not finished:
             b[i][3]=-b[i][3]
         if b[i][1]-b[i][2]<=0 or b[i][1]+b[i][2]>=900:
             b[i][4]=-b[i][4]
-
-
-
+        #и их перерисовки
         circle(screen, c[i], (int(b[i][0]), int(b[i][1])), int(b[i][2]))
+
+    #добавление на экран счета
     text = f.render('Score:' + str(score), 1, (180, 0, 0))
     screen.blit(text, (250, 20))
     pygame.display.update()
@@ -80,14 +89,16 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for i in range (k):
-                if (event.pos[0] - b[i][0]) ** 2 + (event.pos[1] - b[i][1]) ** 2 <= b[i][2] ** 2:
-                    screen.fill(BLACK)
-                    score=score+1
-                    k = k - 1
 
-                    text = f.render('Score:' + str(score), 1, (180, 0, 0))
+                if (event.pos[0] - b[i][0]) ** 2 + (event.pos[1] - b[i][1]) ** 2 <= b[i][2] ** 2:       #реализация уничтожение при клике
+                    screen.fill(BLACK)
+                    score=score+1           #увеличение счета
+                    k = k - 1               #уменьшение количества шариков
+
+                    text = f.render('Score:' + str(score), 1, (180, 0, 0))          #добавление нового счетчика на экран (исправленного)
                     screen.blit(text, (250, 20))
 
+                    #сдвиг всего массива
                     for j in range(i,k):
                         b[j][0] = b[j + 1][0]
                         b[j][1] = b[j + 1][1]
@@ -95,12 +106,14 @@ while not finished:
                         b[j][3] = b[j + 1][3]
                         b[j][4] = b[j + 1][4]
                         c[j] = c[j+1]
+                    #отображение нового набора шаров
                     for s in range(k):
                         circle(screen, c[s], (int(b[s][0]), int(b[s][1])), int(b[s][2]))
                     pygame.display.update()
                     break
-
+    #подготовка к новому начала цикла
     screen.fill(BLACK)
+    #добавление на экран и в массив с информацией нового шара
     if a==30:
         a=0
         new_ball()
