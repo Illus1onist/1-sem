ba34acca3ce3c099a1b06ecf50fd7bff979653e7
Illus1onist <grguliy@mail.ru>
2021-10-28 15:34:33 +0300

    Done

--- lab5/1.py
+++ lab5/1.py
@@ -1,5 +1,5 @@
 import math
-from random import choice
+import random
 
 import pygame
 
@@ -20,6 +20,8 @@ GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 WIDTH = 800
 HEIGHT = 600
 
+global score
+score=0
 
 class Ball:
     def __init__(self, screen: pygame.Surface, x=40, y=450):
@@ -35,7 +37,7 @@ class Ball:
         self.r = 10
         self.vx = 0
         self.vy = 0
-        self.color = choice(GAME_COLORS)
+        self.color = random.choice(GAME_COLORS)
         self.live = 30
 
     def move(self):
@@ -46,8 +48,23 @@ class Ball:
         и стен по краям окна (размер окна 800х600).
         """
         # FIXME
+        g=2.5
+        self.vy = self.vy - g
         self.x += self.vx
         self.y -= self.vy
+        if self.x>=800:
+            self.x=self.x-20
+            self.vx=-self.vx
+        if self.x<=0:
+            self.x=self.x+20
+            self.vx=-self.vx
+        if self.y>=600-10:
+            if abs(self.vx)>=2:
+                self.vy=-self.vy*0.6
+                self.vx=0.7*self.vx
+                self.y=599-10
+            if abs(self.vx) < 2:
+                self.live=0
 
     def draw(self):
         pygame.draw.circle(
@@ -66,7 +83,10 @@ class Ball:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
         # FIXME
+        if (self.x-obj.x)**2+(self.y-obj.y)**2>=(obj.r+self.r)**2:
             return False
+        else:
+            return True
 
 
 class Gun:
@@ -91,8 +111,8 @@ class Gun:
         new_ball = Ball(self.screen)
         new_ball.r += 5
         self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
-        new_ball.vx = self.f2_power * math.cos(self.an)
-        new_ball.vy = - self.f2_power * math.sin(self.an)
+        new_ball.vx = self.f2_power * math.cos(self.an)/FPS*20
+        new_ball.vy = - self.f2_power * math.sin(self.an)/FPS*20
         balls.append(new_ball)
         self.f2_on = 0
         self.f2_power = 10
@@ -107,6 +127,9 @@ class Gun:
             self.color = GREY
 
     def draw(self):
+        l=self.f2_power*0.05
+        pygame.draw.polygon(screen, self.color, [(40,450),(40+l*40*math.cos(self.an),450+l*40*math.sin(self.an)),(40+40*l*math.cos(self.an)+10*math.sin(self.an),450+40*l*math.sin(self.an)-10*math.cos(self.an)),(40+10*math.sin(self.an),450-10*math.cos(self.an)),(40,450)])
+
         # FIXIT don't know how to do it
 
     def power_up(self):
@@ -119,24 +142,38 @@ class Gun:
 
 
 class Target:
+
     # self.points = 0
     # self.live = 1
     # FIXME: don't work!!! How to call this functions when object is created?
     # self.new_target()
 
-    def new_target(self):
+
+    def new_target(self,color):
         """ Инициализация новой цели. """
-        x = self.x = rnd(600, 780)
-        y = self.y = rnd(300, 550)
-        r = self.r = rnd(2, 50)
-        color = self.color = RED
+        x = self.x = random.randint(600, 780)
+        y = self.y = random.randint(300, 550)
+        r = self.r = random.randint(2, 50)
+        self.color = color
+        self.points = 0
+        self.live = 1
 
     def hit(self, points=1):
         """Попадание шарика в цель."""
-        self.points += points
+        global score
+        score=score+1
+
 
     def draw(self):
-        ...
+
+        pygame.draw.circle(screen,self.color,(self.x,self.y),self.r)
+
+#счет
+def Text():
+    f = pygame.font.Font(None, 36)
+    text = f.render('Score:' + str(score), 1, (180, 0, 0))
+    screen.blit(text, (250, 20))
+
 
 
 pygame.init()
@@ -144,19 +181,59 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 bullet = 0
 balls = []
 
+#часы, создание объектов
 clock = pygame.time.Clock()
 gun = Gun(screen)
-target = Target()
+target1 = Target()
+target2 = Target()
 finished = False
+target1.new_target(RED)
+target2.new_target(BLUE)
 
+#таймеры и вспомогательные вещи... (немножко магии)
+l=0
+t=0
+timer1=0
+timer2=0
+#начало великого огромного цикла повторений...
 while not finished:
+    #рисование пушки и экрана
     screen.fill(WHITE)
     gun.draw()
-    target.draw()
+
+    #обработка всяких там таймеров... таймер таргета это обратный отсчет с его уничтожения до его респауна на экране. Все это время на экране висит надпись о том, какой вы молодец)
+    if timer1!=0:
+        timer1=timer1-1
+        f = pygame.font.Font(None, 36)
+        text = f.render('Oh, you are such a noob... You shot a target 1 with ' + str(l) + ' balls', 1,(180, 0, 0))
+        screen.blit(text, (100, 200))
+    if timer1==0:
+        target1.draw()
+        l=0
+
+    if timer2!=0:
+        timer2=timer2-1
+        f = pygame.font.Font(None, 36)
+        text = f.render('Oh, you are such a noob... You shot a target 2 with ' + str(t) + ' balls', 1, (180, 0, 0))
+        screen.blit(text, (100, 250))
+    if timer2==0:
+        target2.draw()
+        t=0
+
     for b in balls:
         b.draw()
+    Text()
+
+    #рисование таргетов, если их таймеры истекли
+    if timer1==0:
+        target1.draw()
+    if timer2==0:
+        target2.draw()
+
     pygame.display.update()
 
+
+    #часы и оператор кликов и отжатий мыши (с добавлением points таргетам, чтобы было понятно, )
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -165,15 +242,41 @@ while not finished:
             gun.fire2_start(event)
         elif event.type == pygame.MOUSEBUTTONUP:
             gun.fire2_end(event)
+            if timer1==0:
+                target1.points = target1.points + 1
+            if timer2==0:
+                target2.points = target2.points + 1
         elif event.type == pygame.MOUSEMOTION:
             gun.targetting(event)
 
+    # перебор всех шариков на экране для их движения и проверки столкновений
+    s=0
     for b in balls:
+        s=s+1
         b.move()
-        if b.hittest(target) and target.live:
-            target.live = 0
-            target.hit()
-            target.new_target()
+
+        #обработка столкновения для обоих целей
+        if timer1==0 and b.hittest(target1) and target1.live:
+
+            target1.live = 0
+            target1.hit()
+            l=target1.points
+            target1.new_target(RED)
+            timer1=3*FPS
+
+
+        if timer2==0 and b.hittest(target2) and target2.live:
+
+            target2.live = 0
+            target2.hit()
+            t=target2.points
+            target2.new_target(BLUE)
+            timer2=3*FPS
+
+
+        if b.live==0:
+            del balls[s-1]
     gun.power_up()
+
 
 pygame.quit()
